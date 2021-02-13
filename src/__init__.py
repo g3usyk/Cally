3a3f8823056ae84c4032c40469be98af9d25bbc8
hsoju <78128765+hsoju@users.noreply.github.com>
2021-02-12 20:21:16 -0500

    Update versioning

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Cal3D Toolkit",
     "author": "hsoju",
-    "version": (1, 0, 4),
+    "version": (1, 0, 5),
     "blender": (2, 90, 0),
     "location": "File > Import-Export",
     "description": "Import-Export Cal3D objects",
