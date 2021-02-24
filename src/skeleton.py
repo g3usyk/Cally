86f0d3487d7ca0a4ce4cc837cca3697b436ce6e1
hsoju <78128765+hsoju@users.noreply.github.com>
2021-02-24 12:07:31 -0500

    Modify ui method name

--- src/skeleton.py
+++ src/skeleton.py
@@ -4,8 +4,8 @@ from .arm.master_root import add_master_root
 
 
 class DefaultSkeleton(bpy.types.Operator):
-    '''Add default IMVU armature'''
-    bl_idname = "armature.imvu_skeleton_add"
+    """Add default IMVU armature"""
+    bl_idname = "object.armature_imvu_skeleton_add"
     bl_label = "IMVU Master Root"
     bl_options = {'REGISTER', 'UNDO'}
 
