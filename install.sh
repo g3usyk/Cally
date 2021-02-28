6e8cbf6fada42f219704cecfeae775ae05ed2199
hsoju <78128765+hsoju@users.noreply.github.com>
2021-02-28 18:00:20 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+VERSION_PREFIX=$(sed -n '4p' __init__.py | cut -c17-23 | sed 's/,/_/g; s/ //g; s/^/v/g')
+VERSION_NAME="cally_${VERSION_PREFIX}"
+mkdir -p dist
 rm -rf dist/*
-mkdir dist/BL-Cal3D
-cp ./__init__.py dist/BL-Cal3D
-cp -r ./src dist/BL-Cal3D
-cd ./dist
-zip -r BL-Cal3D.zip BL-Cal3D
+mkdir dist/"${VERSION_NAME}"
+cp ./__init__.py dist/"${VERSION_NAME}"
+cp -r ./src dist/"${VERSION_NAME}"
+cd ./dist || return
+zip -r "${VERSION_NAME}".zip "${VERSION_NAME}"
+cd ..
