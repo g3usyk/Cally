b088935a94d422de7704508b62c5c2c694b7d790
hsoju <78128765+hsoju@users.noreply.github.com>
2021-03-28 21:46:36 -0400

    fix bug for inconsistent handle creation

--- src/node/pose.py
+++ src/node/pose.py
@@ -3,21 +3,23 @@ import bpy
 
 class Pose:
 
-    def __init__(self, pose_type, context):
+    def __init__(self, pose_type: str, context: bpy.types.Context):
         self.pose_type = pose_type
         if "Spots" not in bpy.data.collections:
             spots = bpy.data.collections.new("Spots")
             context.scene.collection.children.link(spots)
-            layer_col = bpy.context.view_layer.layer_collection.children[spots.name]
-            bpy.context.view_layer.active_layer_collection = layer_col
+        layer_col = context.view_layer.layer_collection.children["Spots"]
+        context.view_layer.active_layer_collection = layer_col
+        if not any([obj.type == 'EMPTY' and obj.empty_display_type == 'SPHERE'
+                    for obj in layer_col.collection.objects]):
             bpy.ops.object.empty_add(type='SPHERE')
-            handle = bpy.context.active_object
+            handle = context.active_object
             handle.name = "Handle"
-            bpy.context.object.empty_display_size = 0.20
+            context.object.empty_display_size = 0.20
 
     def to_scene(self, primitive: str):
         layer_col = bpy.context.view_layer.layer_collection.children["Spots"]
         bpy.context.view_layer.active_layer_collection = layer_col
         bpy.ops.object.empty_add(type=primitive)
         spot = bpy.context.active_object
-        spot.name = f'{self.pose_type}'
+        spot.name = self.pose_type
