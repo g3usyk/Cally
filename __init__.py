3277b6d6bc1f46857de86bbfef82da7e055a12bb
hsoju <78128765+hsoju@users.noreply.github.com>
2021-03-30 15:27:16 -0400

    update versioning

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Cally",
     "author": "hsoju",
-    "version": (4, 0, 1),
+    "version": (4, 0, 2),
     "blender": (2, 90, 0),
     "location": "File > Import-Export",
     "description": "Import-Export Cal3D objects",
