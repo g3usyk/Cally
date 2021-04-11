68664bf991044971927e83f3f06d02b543f4f221
hsoju <78128765+hsoju@users.noreply.github.com>
2021-04-11 19:59:31 -0400

    refactor: move pretty print to utils module

--- src/xfile/utils.py
+++ src/xfile/utils.py
@@ -1,6 +1,23 @@
+import xml.dom.minidom as mini
+
+
 def check_format(filepath: str) -> str:
     with open(filepath, 'rb') as f:
         cal_extension = f.read(3).decode('utf-8')
     if cal_extension in {'CAF', 'CMF', 'CPF', 'CRF', 'CSF'}:
         return cal_extension
     return 'ASCII'
+
+
+def pretty_print(xml_text: str) -> str:
+    """Configures auto generated xml text to a readable format.
+
+    Args:
+        xml_text (str): A string representing an xmf file.
+
+    Returns:
+        An xml string with the text optimized for debugging.
+
+    """
+    dom = mini.parseString(xml_text)
+    return dom.toprettyxml()
